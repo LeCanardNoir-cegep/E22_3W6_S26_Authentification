8bd0a17a0e38d090ae77a0cc086b7cb12ad35a5e
Bruno Pouliot <2168516@cegepmontpetit.ca>
2022-06-30 11:56:11 -0400

    Init

---
+++ S26_Authentification/CrazyBooks_DataAccess/Data/CrazyBooksDbContext.cs
@@ -0,0 +1,36 @@
+﻿using CrazyBooks_Models.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace CrazyBooks_DataAccess.Data
+{
+    public class CrazyBooksDbContext:IdentityDbContext
+    {
+        public CrazyBooksDbContext(DbContextOptions<CrazyBooksDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Subject> Subjects { get; set; }
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Publisher> Publishers { get; set; }
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<AuthorDetail> AuthorsDetail { get; set; }
+        public DbSet<AuthorBook> AuthorsBooks { get; set; }
+        // Identity Entity Framework AppUser ---> root
+        public DbSet<AppUser> SuperUsers { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            //Configuration fluent API
+
+            //composite key
+            modelBuilder.Entity<AuthorBook>().HasKey(ba => new { ba.Author_Id, ba.Book_Id });
+
+            //Générer des données de départ
+            modelBuilder.GenerateData();
+        }
+    }
+}
